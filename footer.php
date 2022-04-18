83408e28e5e00d9ceabc71d47bc9a1106a4c2f62
Henrik Sonnergard <henrik@sonnergard.com>
2022-04-18 17:32:08 +0200

    Removed unneeded tracking, updated jQuery.

--- footer.php
+++ footer.php
@@ -30,24 +30,7 @@
 
   <!-- JS
   ================================================== -->
-  <script type="text/javascript" src="http://susnet.se/susnetstat.js">
-  </script>
-  <script type="text/javascript">
-    susnet_counter_id = 156555;
-    susnet_security_code = '131c58';
-    susnet_node=0;
-    register();
-  </script>
-  <script>
-    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-    ga('create', 'UA-18354976-2', 'sonnergard.com');
-    ga('send', 'pageview');
-  </script> 
-  <script src="https://code.jquery.com/jquery-1.11.2.min.js"></script>
+  <script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ=" crossorigin="anonymous"></script>
   <script src="javascripts/tabs.js"></script>
   <script src="javascripts/responsive-nav.js"></script>
   <script src="javascripts/lightbox.min.js"></script>
